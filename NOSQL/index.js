222556e1d609ecc250062761294d0d17617ddddb
rebeizantoine <139453705+rebeizantoine@users.noreply.github.com>
2023-09-15 09:01:26 +0300

    Update index.js

--- NOSQL/index.js
+++ NOSQL/index.js
@@ -5,13 +5,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// MongoDB connection URL (replace with your MongoDB Atlas URL)
 const mongoURL = 'mongodb+srv://rebeizantoine:rebeizantoine@cluster0.mongodb.net/mydatabase';
 
-// Create a MongoDB client
 const client = new MongoClient(mongoURL, { useUnifiedTopology: true });
 
-// Connect to MongoDB
 async function connectToMongo() {
     try {
         await client.connect();
@@ -23,19 +20,14 @@ async function connectToMongo() {
 
 connectToMongo();
 
-// Middleware to parse JSON request bodies
 app.use(bodyParser.json());
 
-// Create a new user (POST request)
 app.post('/api/users', async (req, res) => {
     try {
-        // Access the MongoDB collection (replace 'users' with your actual collection name)
         const collection = client.db().collection('users');
 
-        // Insert the new user document into the collection
         const result = await collection.insertOne(req.body);
 
-        // Send a success response with the inserted user document
         res.status(201).json(result.ops[0]);
     } catch (error) {
         console.error('Error creating user:', error);
